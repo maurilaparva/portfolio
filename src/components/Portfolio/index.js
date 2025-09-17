c87dcb7691d6780624430e4e84aa1432705dfe40
Mauricio Villavicencio <villa669@umn.edu>
2025-09-17 18:54:05 -0500

    Updated portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -32,11 +32,14 @@ const Portfolio = () => {
                                 <div className="content">
                                     <p className='title'> {port.title}</p>
                                     <h4 className="description">{port.description}</h4>
+                                    {port.url && (
                                     <button
-                                        className='btn'
-                                        onClick={() => window.open(port.url)} >
-                                            View
-                                        </button>
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >
+                                        View
+                                    </button>
+                                    )}
 
                                     <button
                                         className='code-btn'
